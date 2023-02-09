622f817f9419d05ff40046a6a1651b7d06212cc5
D-Khris <63599449+D-Khris@users.noreply.github.com>
2023-02-09 15:43:24 +0100

    Kész a program

--- rps.js
+++ rps.js
@@ -12,6 +12,7 @@ function getPlayerSelection(){
 function playRound(){
     let machine = getComputerChoise();
     let player = getPlayerSelection();
+    let allas = [0,0]
     console.log(machine);
     console.log(player);
     if(machine == player){
@@ -19,14 +20,37 @@ function playRound(){
 
     }else if((player == "rock") && (machine == "sciccors")){
         console.log("Gratulalok nyertel!");
+        allas[0]++;
     }else if((player == "paper") && (machine == "rock")){
         console.log("Gratulalok nyertel!");
+        allas[0]++;
     }else if((player == "sciccors") && (machine == "paper")){
         console.log("Gratulalok nyertel!");
+        allas[0]++;
     }else {
         console.log("Vesztettel!");
+        allas[1]++;
     }
-
+    return allas;
 }
 
-playRound();
+function game(){
+let jatekospont = 0;
+let gepipont = 0;
+let allasnagy = [0,0];
+for(let i=0; i<5; i++){
+    allasnagy = playRound();
+    jatekospont = jatekospont + allasnagy[0];
+    gepipont = gepipont + allasnagy[1];
+    console.log(`Eredmeny állása --> Játékos:${jatekospont} : Gép:${gepipont}`);
+}
+if(jatekospont>gepipont){
+    console.log("GRATULÁKOK!!!!")
+} else if(gepipont>jatekospont) {
+    console.log("VESZTETTEL :((((")
+} else {
+    console.log("DÖNTETTLEN!!!!!")
+}
+}
+
+game();
