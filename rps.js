462bbd1752abd3366330f42c3242e213f7592f33
D-Khris <63599449+D-Khris@users.noreply.github.com>
2023-02-09 13:53:24 +0100

    Kesz a gep valasztasa

---
+++ rps.js
@@ -0,0 +1,7 @@
+function getComputerChoise(){
+    let tomb = ["rock","paper","sciccors"];
+    let valaszt = tomb[Math.floor(Math.random() * 3)];
+    console.log(valaszt);
+}
+
+getComputerChoise();
